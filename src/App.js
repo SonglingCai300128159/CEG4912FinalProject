175b478b095ba9de3483d13d8f8ffaf93e9b15df
Yichen Ye <yye054@uottawa.ca>
2023-10-09 23:44:08 -0400

    change ui to flex
    change ui to flex

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import ENVIRONMENT_MAIN from './components/ENVIRONMENT_MAIN';
 import BATTERY_MAIN from './components/BATTERY_MAIN';
 import FUNCTION_BAR from './components/FUNCTION_BAR';
 
-
-function Header(){
-  return <h1>Hello world</h1>;
-}
-
 function App() {
   return (
     <div className="app">
@@ -18,18 +13,30 @@ function App() {
         <TITLE />
       </header>
       <section className="content">
-        <article className="Left">
-          <div className="Function_Bar">
+        <article className="Function_Bar">
+          <div>
             <FUNCTION_BAR />
           </div>
         </article>
-        <article className="Middle">
-          <div className="Battery_Main"><BATTERY_MAIN /></div>
-          <div className="Camera_Main"><CAMERA_MAIN /></div>
+        <article className="Battery_Main">
+          <div >
+            <BATTERY_MAIN />
+          </div>
         </article>
-        <article className="Right">
-          <div className="Water_Main"><WATER_MAIN /></div>
-          <div className="Environment_Main"><ENVIRONMENT_MAIN /></div>
+        <article  className="Camera_Main">
+          <div>
+            <CAMERA_MAIN />
+          </div>
+        </article>
+        <article className="Water_Main">
+          <div >
+            <WATER_MAIN />
+          </div>
+        </article>
+        <article className="Environment_Main">
+          <div>
+            <ENVIRONMENT_MAIN />
+          </div>
         </article>
       </section>
     </div>
