0ca8cc50f620955230e37a009ececb6a8e649c2f
raywang011116 <xwang503@uottawa.ca>
2023-10-18 23:01:50 -0400

    merge

--- src/components/battery_main.js
+++ src/components/battery_main.js
@@ -1,6 +1,19 @@
+import CircularProgressBarBattery from './CircularProgressBarBattery';
+import { useState } from 'react';
 function BATTERY_MAIN() {
+    const [percentage, setPercentage] = useState(30);
     return (
-        <h2>battery</h2>
+        <div>
+            <h2>battery</h2>
+            <CircularProgressBarBattery percentage={percentage} circleWidth="200" />
+            <div>
+            <input 
+              type="range"
+              min="0"
+              max="100" 
+              step="1" value={percentage} onChange={(ev)=>setPercentage(ev.target.value)}/>
+              </div>
+        </div>
     );
 };
 
--- src/components/water_main.js
+++ src/components/water_main.js
@@ -1,7 +1,16 @@
+import CircularProgressBarWater from './CircularProgressBarWater';
+import { useState } from 'react';
 function WATER_MAIN() {
+    const [percentage, setPercentage] = useState(35);
     return (
         <div>
-            <p>water here</p>       
+            <p>water here</p>
+            <CircularProgressBarWater percentage={percentage} circleWidth="200" />
+            <input 
+              type="range" 
+              min="0" 
+              max="100" 
+              step="1" value={percentage} onChange={(ev)=>setPercentage(ev.target.value)} />
         </div>
     );
 };
