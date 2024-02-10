9fe430d0ff59f7fb9203d374adbd8f9206e96fa5
Je2RYe <yye054@uottawa.ca>
2024-02-10 17:09:16 -0500

    update camera module

---
+++ camera.py
@@ -0,0 +1,16 @@
+import cv2
+import yolov8_to_onnx_webCam
+
+class cam(object):
+    def __init__(self):
+            self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+            self.video.releast()
+    
+    def get_frame(self):
+        success, frame = self.video.read()
+        frame = yolov8_to_onnx_webCam.image_process(frame)
+        frame = yolov8_to_onnx_webCam.output_image(frame, 960, 640)
+        success, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
